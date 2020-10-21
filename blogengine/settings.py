c8b484456cf507619f43a146caea7068a70ab9b7
Alisher <alishalish-777@mail.ru>
2020-10-21 21:57:32 +0600

    [ADD] created a new file and dropped the editable folder and launched a web site with tags inside

--- blogengine/settings.py
+++ blogengine/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog'
+
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'blogengine.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            Path(BASE_DIR/ 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
