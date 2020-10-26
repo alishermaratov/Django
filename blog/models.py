f46481de9ce367734315307274f650b1b4a49b4e
Alisher <alishalish-777@mail.ru>
2020-10-26 18:02:02 +0600

    [ADD]create. теперь можно создавать серез тайтыл и слаг. create

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
 
 
     def __str__(self):
-        return '{}'.format(self.title)
+        return self.title
 
 class Tag(models.Model):
     title = models.CharField(max_length=50)
