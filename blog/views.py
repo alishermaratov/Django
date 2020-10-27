473b66647e72acb1cee98b7c079c7499b993f8f4
Alisher <alishalish-777@mail.ru>
2020-10-27 20:24:17 +0600

    [ADD] новые функции тепереь можно удалять тэги и удалять посты через конпку delete, все делается через кнопку но вызывается в url команды  delete

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.urls import reverse
 from django.shortcuts import get_object_or_404
 from django.views.generic import View
 
@@ -48,6 +49,11 @@ class PostUpdate(ObjectUpdateMixin, View):
     model_form = PostForm
     template = 'blog/post_update_form.html'
 
+class PostDelete(ObjectDeleteMixin, View):
+    model = Post
+    template = 'blog/post_delete_form.html'
+    redirect_url = 'posts_list_url'
+
 
 
 def tags_list(request):
@@ -92,9 +98,19 @@ class TagUpdate(ObjectUpdateMixin, View):
     #     if bound_form.is_valid():
     #         new_tag = bound_form.save()
     #         return redirect(new_tag)
-    #     return render(request, 'blog/tag_update_form.html', context={'form': bound_form, 'tag': tag})
-
-
+    #     return render(request, 'blog/tag_update_form.html', context={'form': bound_form, 'tag': tag}
+class TagDelete(ObjectDeleteMixin, View):
+    model = Tag
+    template = 'blog/tag_delete_form.html'
+    redirect_url = 'tags_list_url'
+    # def get(self, request, slug):
+    #     tag= Tag.objects.get(slug__iexact=slug)
+    #     return render (request, 'blog/tag_delete_form.html', context={'tag': tag})
+    #
+    # def post(self, request, slug):
+    #      tag = Tag.objects.get(slug__iexact=slug)
+    #      tag.delete()
+    #      return redirect(reverse('tags_list_url'))
 
 class TagDetail (ObjectDetailMixin, View):
     model = Tag
