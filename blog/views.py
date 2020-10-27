6a8b203a5fd888ae100da4f27c34046c601fd9f5
Alisher <alishalish-777@mail.ru>
2020-10-27 16:36:12 +0600

    [ADD] теперь можно редактировать старые коды и на каждый код сделали миксины кменьшели код и закачали в Utils. теперь можно каждый под редактировать и добавлять теки Update

--- blog/views.py
+++ blog/views.py
@@ -31,6 +31,7 @@ class PostDetail(ObjectDetailMixin, View):
 class PostCreate(ObjectCreateMixin, View):
     model_form = PostForm
     template = 'blog/post_create_form.html'
+
     # def get(self, request):
     #     form = PostForm()
     #     return render (request, 'blog/post_create_form.html', context={'form':form})
@@ -42,7 +43,10 @@ class PostCreate(ObjectCreateMixin, View):
     #         return redirect(new_post)
     #     return render(request, 'blog/post_create_form.html', context={'form':bound_form})
 
-
+class PostUpdate(ObjectUpdateMixin, View):
+    model = Post
+    model_form = PostForm
+    template = 'blog/post_update_form.html'
 
 
 
@@ -68,10 +72,28 @@ class TagCreate(ObjectCreateMixin, View):
     #
     #     return render(request, 'blog/tag_create.html', context={'form':bound_form})
 
-
 # def tag_detail(request, slug):
 #     tag= Tag.objects.get(slug__iexact=slug)
 #     return render(request, 'blog/tag_detail.html', context={'tag': tag})
+
+class TagUpdate(ObjectUpdateMixin, View):
+    model= Tag
+    model_form = TagForm
+    template = 'blog/tag_update_form.html'
+    # def get(self, request, slug):
+    #     tag= Tag.objects.get(slug__iexact=slug)
+    #     bound_form = TagForm(instance = tag)
+    #     return render (request, 'blog/tag_update_form.html', context={'form': bound_form, 'tag': tag})
+    #
+    # def post(self, request, slug):
+    #     tag = Tag.objects.get(slug__iexact=slug)
+    #     bound_form = TagForm(request.POST, instance = tag)
+    #
+    #     if bound_form.is_valid():
+    #         new_tag = bound_form.save()
+    #         return redirect(new_tag)
+    #     return render(request, 'blog/tag_update_form.html', context={'form': bound_form, 'tag': tag})
+
 
 
 class TagDetail (ObjectDetailMixin, View):
